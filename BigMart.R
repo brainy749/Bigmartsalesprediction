717a1b32400e1a440a140c1d239495b3e248709a
Prakashvanapalli <vanapaliprakash@gmail.com>
2016-03-26 19:42:02 +0530

    My MSE is still very high
    I have completed solving the problem but still my MSE is very high.I have used Linear Regression. Will update it very soon using different model

--- BigMart.R
+++ BigMart.R
@@ -10,112 +10,32 @@ library(ggplot2)
 library(lubridate)
 library(plyr)
 
+#Loading the Training Dataset
 data<- read.csv("Bigmart.csv",sep = ",")
 View(data)
 str(data)
 
-## Scatter plots.
-### Plotting Item_type vs Item_MRP
-ggplot(data, aes(x=Item_Type,y = Item_MRP))+
-  geom_bar(stat = "Identity", width = 0.5)
-
-## Plotting Item_Fat_Content vs Item_Weight
-ggplot(data, aes(x= Item_Fat_Content, y = Item_Weight))+
-  geom_bar(stat= "Identity")
-
-
-## Plotting Outlet_Location_Type vs Item_Outlet_Sales. 
-ggplot(data, aes(x= Outlet_Location_Type, y = Item_Outlet_Sales, fill = Outlet_Type ))+
-  geom_bar(stat = "Identity")
-####Observations:
-#### 1.The tier 1 is occupied by Super market 1 entirely with a Very few grocery store
-#### 2. The tier 2 is entirely occupied with Supermarket type 1 stores.
-#### 3. The Tier 3 market has all other outlets.
-#### 4. Sales Tier 3 > Tier 2 > Tier 1
-
-## Plotting Outlet_Location_Type vs Item_Outlet_Sales and segreegating them into Outlet_Identifier
-ggplot(data, aes(x= Outlet_Location_Type, y = Item_Outlet_Sales, fill = Outlet_Identifier ))+
-  geom_bar(stat = "Identity", position = "dodge")
-####Observations:
-#### 1. The tier 1  has OUT019 , OUT046, OUT049
-#### 2. The tier 2 has OUT017, OUT035, OUT045
-#### 3. The tier 3 has the remaining 4 outlets
-
-## Plotting Outlet_Location_Type vs Item_Outlet_Sales and segreegating them into Outlet_Identifier
-ggplot(data, aes(y= Outlet_Size, x = Outlet_Location_Type, fill = Outlet_Type ))+
-  geom_bar(stat = "Identity", position = "dodge")
-
-ggplot(data, aes(x= Outlet_Size, y = Item_Outlet_Sales, fill = Outlet_Type ))+
-  geom_bar(stat = "Identity", position = "dodge")
-
-ggplot(data, aes(y= Outlet_Location_Type, x = Outlet_Type, fill = Outlet_Size ))+
-  geom_bar(stat = "Identity", position = "dodge")
-#### Observations:
-#### The outlet size of some supermarket type1 and Grocery store were missing and we need to find them.
-#### The outlets opened in 1998, 2002, 2007 were not known.
-#### The outlets opened in 1998 is in tier 3 and the outlets opened in 2002 and 2007 are in tier2.
-#### The outlets opened in 1998 are grocery stores and Supermarkets type1 in 2002 and 2007.
-
-
-
-## Plotting Item_Weight and Item_Type using ggplot
-ggplot(data,aes(x=Item_Type , y = Item_Identifier))+
-  geom_bar(stat = "Identity")
-
-
-
-
-
-## Data Cleaning 
-## Problems.
-
-### 1. Converting low fat and LF to Low Fat
-data$Item_Fat_Content <- revalue(data$Item_Fat_Content, c("LF"="Low Fat", "low fat"="Low Fat", "reg"="Regular"))
-
-
-### 2 . Changing the Outlet_Establishment_year from Integer to Factor
-data$Outlet_Establishment_Year <- as.factor(data$Outlet_Establishment_Year)
-
-### 3. Finding the missing values in Item_Weight
-
-r<- data[0,]
-data$Item_Identifier <- as.integer(data$Item_Identifier)
-for (i in 1:1559){
-  m1<- filter(data, Item_Identifier == i)
-  m2 <- filter(m1, is.na(Item_Weight)== T)
-  m3 <- filter(m1, is.na(Item_Weight)== F)
-  f1 <- nrow(m2)
-  f2 <- nrow(m3)
-  if (f1>=1 & f2 >= 1){
-    m2$Item_Weight[1:f1] <- mean(m3$Item_Weight)
-  } else{
-    m2 <- m2
-    m3 <- m3
-  }
-  r <- rbind(r,m2,m3)
-  m2 <- Null
-  m3 <- Null
-}
-
-## there are 4 missing values in the Item_Weight yet and we will check, Weather this can be removed or not.
-
-## Test data
+#Loading the Testing dataset
 datatest<- read.csv("Bigmarttest.csv",sep = ",")
 
-##cleaning datatest as usually done for training set.
-### 1. Converting low fat and LF to Low Fat
-datatest$Item_Fat_Content <- revalue(datatest$Item_Fat_Content, c("LF"="Low Fat", "low fat"="Low Fat", "reg"="Regular"))
+# Cleaning of Data
 
+## Combaining the both.
+### Reason for Combaining is
+#### . The Products weights are missing and Inorder to find them, We need to convert the Item_Identifier into 
+#### Numeric and assign variables accordingly to wieghts. When we do the process for different datasets.
+#### We still left-out with different NA's . Some products in train which have missing values are there in 
+#### test data and vice-versa.
 
-### 2 . Changing the Outlet_Establishment_year from Integer to Factor
-datatest$Outlet_Establishment_Year <- as.factor(datatest$Outlet_Establishment_Year)
+## Adding the prediction (sales column) to test data.
+datatest <- mutate(datatest, Item_Outlet_Sales = 0)
+data_clean <- rbind(data,datatest)
 
-### 3. Finding the missing values in Item_Weight
-
-rtest<- datatest[0,]
-datatest$Item_Identifier <- as.integer(datatest$Item_Identifier)
-for (i in 1:1543){
-  m1<- filter(datatest, Item_Identifier == i)
+## Missing values of Item_Identifier
+rtest<- data_clean[0,]
+data_clean$Item_Identifier <- as.numeric(levels(data_clean$Item_Identifier))
+for (i in 1:1559){
+  m1<- filter(data_clean, Item_Identifier == i)
   m2 <- filter(m1, is.na(Item_Weight)== T)
   m3 <- filter(m1, is.na(Item_Weight)== F)
   f1 <- nrow(m2)
@@ -130,5 +50,70 @@ for (i in 1:1543){
   m2 <- Null
   m3 <- Null
 }
+data_clean <- rtest
 
-m1<- filter(datatest, Item_Identifier == 1200)
+
+## Chaning LF and low to Low Fat and reg to Regular
+data_clean$Item_Fat_Content <- revalue(data_clean$Item_Fat_Content, c("LF"="Low Fat", "low fat"="Low Fat", "reg"="Regular"))
+
+# Dividing the data according to Outlet_size in Grocery segment.
+## The Visibility index is same for both the outlets and so all the Grocery stores can be assigned to small
+## stores
+
+#Plots:
+
+
+## Plotting all the graphs we get,
+data_clean$Outlet_Size[data_clean$Outlet_Type == "Grocery Store" & data_clean$Outlet_Establishment_Year == 1998] <- "Small"
+data_clean$Outlet_Size[data_clean$Outlet_Type == "Supermarket Type1" & data_clean$Outlet_Establishment_Year == 2002] <- "High"
+data_clean$Outlet_Size[data_clean$Outlet_Type == "Supermarket Type1" & data_clean$Outlet_Establishment_Year == 2007] <- "High"
+summary(data_clean)
+
+#Dividing the data_clean again into datatest and data(train). 
+data1<- filter(data_clean, Item_Outlet_Sales != 0)
+datatest1 <- filter(data_clean, Item_Outlet_Sales == 0)
+summary(data1)
+summary(datatest1)
+str(data1)
+
+## Correlation Matrix.
+library(corrplot)
+corr_matrix = cor(data1[,c(1,2,4,6,8,12)])
+corrplot(corr_matrix,order ="hclust",method="number")
+## Only Item_MRP is significant with r = 0.57
+
+## Feature Scaling 
+data2 <- mutate_each_(data1,funs(scale),vars=c("Item_MRP","Item_Visibility","Item_Weight"))
+
+## Now checking the correlation Matrix
+library(corrplot)
+corr_matrix = cor(data2[,c(2,4,6,12)])
+corrplot(corr_matrix,order ="hclust",method="number")
+
+## Plotting Item_Outlet_Sales and Item_MRP
+ggplot(data2,aes(x=Item_MRP, y = log(Item_Outlet_Sales)))+
+  geom_point()+
+  facet_wrap(~Outlet_Identifier)
+
+## Linear Regression equation 
+equation1 <- lm(log(Item_Outlet_Sales) ~ Item_MRP+Outlet_Identifier,data2)
+summary(equation1)
+
+##R2 and Adjusted R2 - Both are coming around and 72% 
+## checking for RMSE
+
+## Test-Data
+data1test <- mutate_each_(datatest1,funs(scale),vars=c("Item_MRP","Item_Visibility","Item_Weight"))
+
+predicted_y = predict(equation1,data1test)
+predicted_y = exp(predicted_y)
+datatest2 = mutate(data1test , Item_Outlet_Sales = predicted_y)
+datatest2=datatest2[,c(1,7,12)]
+
+rawdata<- datatest[order(datatest$Item_Identifier),]
+rawdata_estimation <- datatest2[order(datatest2$Item_Identifier),]
+rawdata_estimation$Item_Identifier <- rawdata$Item_Identifier
+
+## Writing the final dataset to the directory.
+write.csv(rawdata_estimation, "Bigmartpredictionsfinal.csv")
+
